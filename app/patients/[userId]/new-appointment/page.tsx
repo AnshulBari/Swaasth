e929e5fd8b45b5713b277cba900911a2abc30f28
Anshul Bari <anshulbari1@gmail.com>
2025-02-22 18:29:28 +0530

    tweaks

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -7,35 +7,36 @@ const Appointment = async ({ params: { userId } }: SearchParamProps) => {
   const patient = await getPatient(userId);
 
   return (
-    <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[860px] flex-1 justify-between">
-          <Image
-            src="/assets/icons/logo-full.svg"
-            height={1000}
-            width={1000}
-            alt="logo"
-            className="mb-12 h-10 w-fit"
-          />
-
-          <AppointmentForm
-            patientId={patient?.$id}
-            userId={userId}
-            type="create"
-          />
-
-          <p className="copyright mt-10 py-12">© 2025 Swaasth</p>
-        </div>
-      </section>
-
+    <div className="flex h-screen max-h-screen overflow-hidden">
+  <section className="remove-scrollbar container my-auto flex-1 overflow-hidden">
+    <div className="sub-container max-w-[860px] flex-1 justify-between">
       <Image
-        src="/assets/images/appointment-img.png"
-        height={1500}
-        width={1500}
-        alt="appointment"
-        className="side-img max-w-[390px] bg-bottom"
+        src="/assets/icons/logo-full.svg"
+        height={1000}
+        width={1000}
+        alt="logo"
+        className="mb-12 h-10 w-fit"
       />
+
+      <AppointmentForm
+        patientId={patient?.$id}
+        userId={userId}
+        type="create"
+      />
+
+      <p className="copyright mt-10 py-12">© 2025 Swaasth</p>
     </div>
+  </section>
+
+  <Image
+    src="/assets/images/appointment-img.jpeg"
+    height={1500}
+    width={1500}
+    alt="appointment"
+    className="side-img max-w-[400px] bg-bottom object-cover"
+  />
+</div>
+
   );
 };
 
